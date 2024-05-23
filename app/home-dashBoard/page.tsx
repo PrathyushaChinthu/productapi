686ee435e775d75a95dbce571f07ccda7c6785df
Prathyusha <chinthakuntaprathyusha2001@gmail.com>
2024-05-23 12:28:22 +0530

    add:brands to home page

--- app/home-dashBoard/page.tsx
+++ app/home-dashBoard/page.tsx
@@ -1,9 +1,68 @@
+'use client';
 import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
+import { Grid, Card, Typography } from '@mui/material';
+import { IBrand } from '../types/brand';
+import { useLazyQuery } from '@apollo/client';
+import { FIND_BRANDS } from '@/app/graphql/brand';
 
 type Props = {};
 
 const HomeDashBoard = (props: Props) => {
-  return <div>HomeDashBoard</div>;
+  const [brands, setBrands] = useState<IBrand[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [findBrands, { loading: brandsLoading }] = useLazyQuery(FIND_BRANDS, {
+    fetchPolicy: 'no-cache',
+    onCompleted: (data) => {
+      const { brands: brandsData = [] } = data?.findBrands || {
+        brands: [],
+      };
+      setBrands(brandsData);
+      setLoading(false);
+    },
+  });
+
+  const fetchFilters = useCallback(() => {
+    setLoading(true);
+    findBrands({ variables: { limit: 100 } });
+  }, [findBrands]);
+
+  useEffect(() => {
+    fetchFilters();
+  }, [fetchFilters]);
+
+  return (
+    <Grid container spacing={2}>
+      {brands.length > 0 ? (
+        brands.map((brand) => (
+          <Grid
+            item
+            key={brand.id}
+            xs={12}
+            md={3}
+            sx={{
+              marginBottom: 1,
+              marginTop: 1,
+            }}
+          >
+            <Card
+              key={brand.id}
+              sx={{
+                padding: 1,
+                height: '100%',
+              }}
+            >
+              <Typography>{brand.name}</Typography>
+            </Card>
+          </Grid>
+        ))
+      ) : (
+        <Typography>No data found</Typography>
+      )}
+    </Grid>
+  );
 };
 
 export default HomeDashBoard;
